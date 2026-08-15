be8f54b0c508976303862e5100bad997a4fb78c3
Edilee686 <134784971+Edilee686@users.noreply.github.com>
2026-08-15 16:42:08 +0300

    Delete flutter_app directory

--- flutter_app/android/app/src/main/kotlin/com/glances/app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.glances.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- flutter_app/lib/models/chat_message.dart
+++
@@ -1,6 +0,0 @@
-class ChatMessage {
-  const ChatMessage({required this.text, required this.mine, required this.sentAt});
-  final String text;
-  final bool mine;
-  final DateTime sentAt;
-}
